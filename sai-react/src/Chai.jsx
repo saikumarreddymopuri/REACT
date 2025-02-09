7bb4abda92aec013f3a89e597e997e66f1b29d26
saikumarredyymopuri <saikumarmopuri@gmail.com>
2025-02-09 23:20:06 +0530

    creat element & custom react

---
+++ sai-react/src/Chai.jsx
@@ -0,0 +1,12 @@
+function Chai(){
+    return(
+        <>
+        <h1>vite + react is ready enjoy pandagowww</h1>
+        <h2>chai is ready</h2>
+        </>
+        
+    )
+}
+
+
+export default Chai
